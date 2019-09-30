18fd07e79917fbe7a3b25b43dcaf1ba5bc19b95f
Enya <enya.baez@gmail.com>
2019-09-30 13:15:13 -0400

    map not working yet

--- public/bundle.js
+++ public/bundle.js
@@ -104,7 +104,7 @@ eval("/* Mapbox GL JS is licensed under the 3-Clause BSD License. Full text of l
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("console.log(\"The index.js file is running!\");\nconst mapboxgl = __webpack_require__(/*! mapbox-gl */ \"./node_modules/mapbox-gl/dist/mapbox-gl.js\");\n\nmapboxgl.accessToken = \"pk.eyJ1Ijoib2xlc2lhLWFnIiwiYSI6ImNrMTZtZWlrcjAwc2QzbHFwZmNldG44c2gifQ.zu7TDpNCyoLvMzQfwxdLyA\";\n\nconst map = new mapboxgl.Map({\n  container: \"map\",\n  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago\n  zoom: 12, // starting zoom\n  style: \"mapbox://styles/mapbox/streets-v10\" // mapbox has lots of different map styles available.\n});\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("console.log(\"The index.js file is running!\");\nconst mapboxgl = __webpack_require__(/*! mapbox-gl */ \"./node_modules/mapbox-gl/dist/mapbox-gl.js\");\n\nmapboxgl.accessToken = \"pk.eyJ1Ijoib2xlc2lhLWFnIiwiYSI6ImNrMTZtZWlrcjAwc2QzbHFwZmNldG44c2gifQ.zu7TDpNCyoLvMzQfwxdLyA\";\n\nlet map = new mapboxgl.Map({\n  container: \"map\",\n  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago\n  zoom: 12, // starting zoom\n  style: \"mapbox://styles/mapbox/streets-v10\" // mapbox has lots of different map styles available.\n});\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
